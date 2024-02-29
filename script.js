69a8018468c36460d95dc2bd1fd6e2d1efad5ab4
Pranav <pranavjathar28@gmail.com>
2024-02-29 20:00:13 +0530

    reCAPTCHA Changes

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function beforesubmit()
-{
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log(inputdate.value); //string --> date (en_IN)
+function beforesubmit() {
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
+  console.log(inputdate.value); //string --> date (en_IN)
 
-    let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = formatedDate;
+  let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  outputdate.value = formatedDate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
