b9a53b47210ae0f725bfdbb2ddf3ed1ae7de1e49
Pranav <pranavjathar28@gmail.com>
2024-02-29 20:08:46 +0530

    reCAPTCHA Validations

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function beforesubmit() {
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
-  console.log(inputdate.value); //string --> date (en_IN)
+let captachcheked = false;
 
-  let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  outputdate.value = formatedDate;
+function beforesubmit(event) {
+    if(captachcheked)
+    {
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log(inputdate.value); //string --> date (en_IN)
+      
+        let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value = formatedDate;
+    }
+    else
+    {
+        alert('To submit the lead, kindly verify the reCAPTCHA box.');
+        event.preventDefault(); //this will stop the submission of form
+    }
 }
 
 function timestamp() {
@@ -19,3 +29,8 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function catchasuccess()
+{
+    captachcheked = true;
+}
